81c63da27ffdc67b26a303e827a57accdffed981
Pelozo <el.pelozo@gmail.com>
2021-11-13 17:14:53 -0300

    fix merge requests

--- user/views.py
+++ user/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView
 
 
 # silvania 1234
-class login(LoginView):
+class Login(LoginView):
     template = 'login.html'
+
+
+class Register(CreateView):
+    template = 'register.html'
+    success_message = "Your profile was created successfully"
