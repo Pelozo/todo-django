50a4f25529b694f8242acb10d5cb0891ba663672
Pelozo <el.pelozo@gmail.com>
2021-11-13 18:01:24 -0300

    user registration? idk rick

--- user/views.py
+++ user/views.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.views import LoginView
 from django.views.generic.edit import CreateView
+from user.models import SystemUser
+from django.urls import reverse
 
 
 # silvania 1234
 class Login(LoginView):
+    model = SystemUser
     template = 'login.html'
 
 
 class Register(CreateView):
-    template = 'register.html'
-    success_message = "Your profile was created successfully"
+    model = SystemUser
+    #fields = '__all__'
+    fields = ['username', 'email', 'first_name', 'last_name', 'password']
+    template_name = 'registration/register.html'
+
+    def get_success_url(self):
+        return reverse('home')
